7dc366c51f7c24be4773bc66e789b4b20d6e0aff
firdavsyuswiut <site.com.uz@gmail.com>
2026-09-10 11:34:58 +0500

    First Lesson start

---
+++ lesson1/lesson1.java
@@ -0,0 +1,57 @@
+/*
+Primitive | Wrapper Class
+_________________________
+byte      | Byte
+short     | Short
+char      | Character
+int       | Integer
+long      | Long
+float     | Float
+double    | Double
+boolean   | Boolean
+*/
+
+// Declaration Statement
+
+//1. Data type: The type of the variable (e.g., int, String, double, etc.).
+//2. Variable name: The name (or identifier) you give to the variable.
+
+int age; // Declares an integer variable named "age"
+String name; // Declares a String variable named "name"
+double price; // Declares a double variable named "price"
+
+int age = 25; // Declares and initializes "age" to 25
+String name = "Maria"; // Declares and initializes "name" to "John"
+
+
+//Example of method declaration:
+public void greet() {
+    System.out.println("Hello!");
+}
+
+//Example of class declaration:
+public class Student {
+    // Class body
+}
+
+int myFirstNumber = ( 18 - 10) * ( 45 / 5);
+
+
+/*
+Operator Numeric types  char            boolean   String
++       Addition        Addition        n/a       Concatenation
+-       Subtraction     Subtraction     n/a        n/a
+*       Multiplication  Multiplication  n/a        n/a
+/       Division        Division        n/a        n/a
+%    Remainder(Modulus) Remainder(Modulus) n/a n/a
+
+
+• The abbreviating operators:
+Shorthand Operator                  Code Sample
+Post-fix Increment Operator         result++;
+Post-fix Decrement Operator         result--;
+Addition Compound Assignment        result += 5;
+Subtraction Compound Assignment     result -= 5;
+Multiplication Compound Assignment  result *= 5;
+Division Compound Assignment        result /= 5;
+ */
